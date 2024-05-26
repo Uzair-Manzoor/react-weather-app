716e0aac69b717b979d22a20e1812301902169c3
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-05-26 15:43:01 +0500

    add weather api

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5095fb86eamsh37c596009fa59fbp1966b8jsnb5ebd8170ed9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e020bb36cc8b96f8571b02cde417db0d";
